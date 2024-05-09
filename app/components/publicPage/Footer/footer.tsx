da68a21c46c67f383101db80f001f782d07759a1
Majdanka <majdan_k@onet.pl>
2024-05-09 12:12:33 +0200

    HomePage container

--- app/components/publicPage/Footer/footer.tsx
+++ app/components/publicPage/Footer/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
-    return (
-        <footer className="flex justify-center flex-col md:flex-row bg-gradient-to-r from-yellow-200 to-green-200 rounded-full m-1 h-16 items-center">
-            &copy; 2024 My Company. All rights reserved.
-        </footer>
-    );
+  return (
+    <footer className="flex justify-center flex-col md:flex-row bg-gradient-to-r from-yellow-200 to-green-200 rounded-full m-1 h-16 items-center w-full">
+      &copy; 2024 My Company. All rights reserved.
+    </footer>
+  );
 }
--- app/components/publicPage/Header/header.tsx
+++ app/components/publicPage/Header/header.tsx
@@ -3,19 +3,19 @@ import NavBar from "./navBar";
 import Link from "next/link";
 
 export default function Header() {
-    return (
-        <header className="flex justify-center flex-col md:flex-row bg-gradient-to-r from-green-200 to-yellow-200 rounded-full m-1 h-16 items-center">
-            <div className="flex justify-evenly w-2/3">
-                <div className="flex h-16 align-middle w-full pl-1 items-center">
-                    <Logo />
-                    <Link href='../..'>
-                        <h1 className="ml-3 text-xl font-bold">Personal site</h1>
-                    </Link>
-                </div>
-            </div>
-            <nav className="flex justify-evenly w-1/3 font-semibold">
-                <NavBar />
-            </nav>
-        </header>
-    );
+  return (
+    <header className="flex justify-center flex-col md:flex-row bg-gradient-to-r from-green-200 to-yellow-200 rounded-full m-1 h-16 items-center w-full">
+      <div className="flex justify-evenly w-2/3">
+        <div className="flex h-16 align-middle w-full pl-1 items-center">
+          <Logo />
+          <Link href="../..">
+            <h1 className="ml-3 text-xl font-bold">Personal site</h1>
+          </Link>
+        </div>
+      </div>
+      <nav className="flex justify-evenly w-1/3 font-semibold">
+        <NavBar />
+      </nav>
+    </header>
+  );
 }
