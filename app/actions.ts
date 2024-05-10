63fda7c44b0ff8e1f5a186ef4af51cbe2ea6e057
Majdanka <majdan_k@onet.pl>
2024-05-10 13:16:07 +0200

    Aside post fetching

--- app/actions.ts
+++ app/actions.ts
@@ -1,20 +1,10 @@
 import prisma from "./prisma";
 
-export async function getPosts() {
+export async function getRecentPosts() {
   return await prisma.post.findMany({
     take: 5,
     orderBy: {
       createdAt: "desc",
-    },
-  });
-}
-
-export async function addPost(title: string, content: string, authorId: number) {
-    return await prisma.post.create({
-    data: {
-      title,
-      content,
-      authorId
     },
   });
 }
