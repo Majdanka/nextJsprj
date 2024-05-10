c84b6ced51b2f2c39e51ae5972b4f3d9f2d8433c
Majdanka <majdan_k@onet.pl>
2024-05-10 13:31:48 +0200

    Simple responsive styling for non important pages

--- app/(publicPage)/contact/page.tsx
+++ app/(publicPage)/contact/page.tsx
@@ -1,13 +1,13 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Contact'
+  title: "Contact",
 };
 
 export default function Contact() {
   return (
-    <main>
-      ContactPage
+    <main className="w-full md:w-[99%] bg-orange-300 rounded-3xl md:h-[80vh] flex flex-col items-center h-[130vh] justify-center">
+      <h1 className="text-5xl font-bold">Contact Page</h1>
     </main>
   );
 }
