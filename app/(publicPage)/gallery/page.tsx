8724531d8b72b317bffd0d04a710bea3e807f8c6
Majdanka <majdan_k@onet.pl>
2024-05-14 11:25:43 +0200

    Suspense addition for the galleryPage

--- app/(publicPage)/gallery/page.tsx
+++ app/(publicPage)/gallery/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import GalleryHandler from "@/app/components/galleryPage/galleryHandler";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Gallery",
@@ -9,7 +10,9 @@ export default function Gallery() {
   return (
     <main className="w-[99%] bg-orange-300 rounded-3xl md:h-[80vh] flex flex-col items-center h-[130vh]">
       <h1 className="text-4xl font-bold pt-2">Gallery</h1>
-      <GalleryHandler />
+      <Suspense fallback="Loading gallery... ">
+        <GalleryHandler />
+      </Suspense>
     </main>
   );
 }
