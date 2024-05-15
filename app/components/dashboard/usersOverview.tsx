e5e90edaf10af5ac9d7e968f675b954c88db790b
Majdanka <majdan_k@onet.pl>
2024-05-15 14:08:51 +0200

    users overview for small amount and heading delete

--- app/components/dashboard/usersOverview.tsx
+++ app/components/dashboard/usersOverview.tsx
@@ -6,7 +6,7 @@ export default async function UsersOverview() {
 
   const map = [];
 
-  if (authors.length > 0) {
+  if (authors.length > 6) {
     for (let i = 0; i < 5; i++) {
       const author = authors[0];
       const count = await fetchAuthorsPostsCount(author.id);
@@ -32,6 +32,22 @@ export default async function UsersOverview() {
       </Link>
     );
   } else {
+    for (let i = 0; i < authors.length; i++) {
+      const author = authors[i];
+      const count = await fetchAuthorsPostsCount(author.id);
+      map.push(
+        <Link
+          href={`/dashboard/users/${author.id}`}
+          key={author.id}
+          className="w-[46%] border rounded-3xl my-1 py-3 text-gray-700 border-gray-700 pl-2 bg-slate-200 hover:bg-slate-300 cursor-pointer flex flex-col font-[500]"
+        >
+          <div className="w-full flex flex-col justify-center items-center">
+            {"Author name: " + author.userName}
+            <br></br> {"Written posts: " + count}
+          </div>
+        </Link>
+      );
+    }
   }
 
   return <>{map}</>;
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,24 +6,22 @@ import Link from "next/link";
 export default function Dashboard() {
   return (
     <>
-      <div className="ml-3 mt-1 w-full h-[1.4rem]">
-        <h1 className="text-gray-800 font-bold text-[1.35rem]">Overview</h1>
-      </div>
-      <fieldset className="mt-2 w-[98%] border border-gray-700 rounded-3xl bg-slate-300 h-[48vh] flex-col flex items-center">
+      <fieldset className="mt-2 w-[98%] border border-gray-700 rounded-3xl bg-slate-300 h-[50vh] flex-col flex items-center">
         <legend className="ml-2 text-gray-700 font-bold text-[1.2rem]">
           Latest posts
         </legend>
         <div className="mt-2 w-full flex-col flex items-center">
           <AddPostButton />
           <LastPostsDash />
-          <div className="w-[96%] border rounded-3xl my-1 py-2 text-gray-700 hover:text-blue-900 border-gray-700 pl-2 bg-blue-200 hover:bg-blue-300 cursor-pointer flex font-[500] items-center justify-center">
-            <Link href={`/dashboard/posts/`}>
-              <div>{"->" + "View all posts" + "<-"}</div>
-            </Link>
-          </div>
+          <Link
+            href={`/dashboard/posts/`}
+            className="w-[96%] border rounded-3xl my-1 py-2 text-gray-700 hover:text-blue-900 border-gray-700 pl-2 bg-blue-200 hover:bg-blue-300 cursor-pointer flex font-[500] items-center justify-center"
+          >
+            <div>{"->" + "View all posts" + "<-"}</div>
+          </Link>
         </div>
       </fieldset>
-      <fieldset className="mt-2 w-[98%] border border-gray-700 rounded-3xl bg-slate-300 h-[42vh] flex-col flex items-center">
+      <fieldset className="mt-2 w-[98%] border border-gray-700 rounded-3xl bg-slate-300 h-[44vh] flex-col flex items-center">
         <legend className="ml-2 text-gray-700 font-bold text-[1.2rem]">
           Users
         </legend>
