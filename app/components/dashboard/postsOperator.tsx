16c1d427c2af57a6d8d85f2a820697df4757c1dd
Majdanka <majdan_k@onet.pl>
2024-05-16 13:15:39 +0200

    post deleting fix

--- app/components/dashboard/postsOperator.tsx
+++ app/components/dashboard/postsOperator.tsx
@@ -3,6 +3,7 @@ import { Suspense } from "react";
 import Search from "../blogPage/search";
 import PostBlock from "./postBlock";
 import Pagination from "../blogPage/pagination";
+import Link from "next/link";
 
 export default async function PostsOperator({
   searchParams,
@@ -11,12 +12,18 @@ export default async function PostsOperator({
 }) {
   const term = searchParams?.search || "";
   const page = Number(searchParams?.page) || 1;
-  const totalPages = await fetchPostsPages(term, 10);
-  const posts = await fetchPosts({ title: term, page: page, take: 10 });
+  const totalPages = await fetchPostsPages(term, 8);
+  const posts = await fetchPosts({ title: term, page: page, take: 8 });
 
   return (
     <>
       <Search placeholder="Search posts..." />
+      <Link
+        href={`/dashboard/posts/add`}
+        className="w-[98%] border mt-1 rounded-3xl text-gray-700 hover:text-green-900 border-gray-700 pl-2 bg-green-200 hover:bg-green-300 cursor-pointer flex font-[500] items-center justify-center"
+      >
+        (+) Add new post
+      </Link>
       <Suspense fallback="Loading...">
         <div className="flex flex-col pt-2 items-center w-full">
           {posts.map((post) => (
