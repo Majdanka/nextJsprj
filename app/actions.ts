ffdb14123dce0d0c529c77d15693ccb68ad3cf73
Majdanka <majdan_k@onet.pl>
2024-05-17 11:33:12 +0200

    post adding fix

--- app/actions.ts
+++ app/actions.ts
@@ -2,6 +2,8 @@
 
 import prisma from "./prisma";
 import { unstable_noStore as noStore } from "next/cache";
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 
 export async function fetchRecentPosts({ take } : { take: number}) {
   noStore();
@@ -84,12 +86,22 @@ export async function deletePostWithId({id} : {id: number | undefined}) {
   }
 }
 
-export async function addPostDb({ title, content, authorId } : { title: string, content: string, authorId: number }) {
-  return await prisma.post.create({
+export async function addPost(formData: FormData) {
+  let title = formData.get("title")?.toString()
+  let content = formData.get("content")?.toString()
+  if(!title || !content) {
+    throw new Error("Title and content are required")
+  }
+  const { authorId } = { authorId: 1 };
+  await prisma.post.create({
     data: {
       title,
       content,
       authorId
     }
   });
+
+  revalidatePath("/dashboard");
+  revalidatePath("/dashboard/posts");
+  redirect("/dashboard/posts");
 }
