99a18b0870e4c462a3bc390b3bfa6bbf2d0b72c5
Majdanka <majdan_k@onet.pl>
2024-05-21 14:03:10 +0200

    User edit page authorized dependent content scheme

--- app/dashboard/(breadCrumbsPages)/users/[userId]/edit/page.tsx
+++ app/dashboard/(breadCrumbsPages)/users/[userId]/edit/page.tsx
@@ -1,3 +1,27 @@
-export default function UsersEdit() {
-  return <></>;
+import { cookies } from "next/headers";
+import { fetchAuthorById } from "@/app/actions";
+import Link from "next/link";
+
+export default async function UsersEdit({
+  params,
+}: {
+  params: { userId: string };
+}) {
+  const store = cookies();
+  const username = store.get("userName")?.value;
+  const author = await fetchAuthorById({ id: Number(params.userId) });
+
+  if (username !== author?.userName) {
+    return (
+      <>
+        <h1 className="">Unauthorized</h1>
+        <Link href={`/dashboard/users/${params.userId}`}>
+          Check the user overview
+        </Link>
+        <Link href="/dashboard/users">Go back to browsing users</Link>
+      </>
+    );
+  } else {
+    return <h1>Authorized</h1>;
+  }
 }
