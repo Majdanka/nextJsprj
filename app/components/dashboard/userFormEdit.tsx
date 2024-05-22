bd574e3e82b5a3b863618c6aa035535b1109440c
Majdanka <majdan_k@onet.pl>
2024-05-22 12:08:17 +0200

    Username and password changing functions

--- app/components/dashboard/userFormEdit.tsx
+++ app/components/dashboard/userFormEdit.tsx
@@ -1,3 +1,86 @@
-export default function UserFormEdit() {
-  return <></>;
+import { changeUsername, changePassword } from "@/app/actions";
+
+export default function UserFormEdit({
+  author,
+}: {
+  author: {
+    id: number;
+    userName: string;
+    password: string;
+  } | null;
+}) {
+  return (
+    <div className="flex justify-center items-center flex-col w-full text-xl font-semibold">
+      <form
+        action={changeUsername}
+        className="border border-green-500 rounded-3xl p-5 my-3"
+      >
+        <div className="flex flex-col mb-3">
+          <label htmlFor="username" className="text-green-600">
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            autoComplete="off"
+            required
+          />
+          <div className="flex flex-col my-3">
+            <label htmlFor="password" className="text-green-600">
+              Current password
+            </label>
+            <input
+              type="password"
+              id="passwordForUsername"
+              name="password"
+              required
+            />
+          </div>
+        </div>
+        <input type="hidden" name="current" id="current" value={author?.id} />
+        <button
+          type="submit"
+          className="border-2 border-green-500 rounded-3xl w-full py-1 bg-green-300 hover:bg-green-400"
+        >
+          Change username
+        </button>
+      </form>
+      <form
+        action={changePassword}
+        className="border border-green-500 rounded-3xl p-5 my-3"
+      >
+        <div className="flex flex-col mb-3">
+          <label htmlFor="password" className="text-green-600">
+            Current password
+          </label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <div className="flex flex-col my-3">
+          <label htmlFor="newpassword" className="text-green-600">
+            New password
+          </label>
+          <input type="password" id="newpassword" name="newpassword" required />
+        </div>
+        <div className="flex flex-col my-3">
+          <label htmlFor="reppassword" className="text-green-600">
+            Repeat password
+          </label>
+          <input type="password" id="reppassword" name="reppassword" required />
+        </div>
+        <input
+          type="hidden"
+          name="current"
+          id="currentForPass"
+          value={author?.id}
+        />
+        <button
+          type="submit"
+          className="border-2 border-green-500 rounded-3xl w-full py-1 bg-green-300 hover:bg-green-400"
+        >
+          Change password
+        </button>
+      </form>
+    </div>
+  );
 }
--- app/dashboard/(breadCrumbsPages)/users/[userId]/page.tsx
+++ app/dashboard/(breadCrumbsPages)/users/[userId]/page.tsx
@@ -1,3 +1,13 @@
-export default function Users() {
-  return <></>;
+import SpecUserOverview from "@/app/components/dashboard/specUserOverview";
+
+export default async function Users({
+  params,
+}: {
+  params: { userId: string };
+}) {
+  return (
+    <div className="h-screen w-full flex items-center justify-evenly flex-col text-3xl font-semibold">
+      <SpecUserOverview id={Number(params.userId)} />
+    </div>
+  );
 }
