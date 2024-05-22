1aad7c57ce65d525957d9632fca5be9afe36912b
Majdanka <majdan_k@onet.pl>
2024-05-22 12:33:12 +0200

    User review page adjusting

--- app/dashboard/(breadCrumbsPages)/users/[userId]/page.tsx
+++ app/dashboard/(breadCrumbsPages)/users/[userId]/page.tsx
@@ -1,13 +1,28 @@
 import SpecUserOverview from "@/app/components/dashboard/specUserOverview";
+import { cookies } from "next/headers";
+import { fetchAuthorById } from "@/app/actions";
+import Link from "next/link";
 
 export default async function Users({
   params,
 }: {
   params: { userId: string };
 }) {
+  const username = cookies().get("userName")?.value;
+  const author = await fetchAuthorById({ id: Number(params.userId) });
   return (
-    <div className="h-screen w-full flex items-center justify-evenly flex-col text-3xl font-semibold">
-      <SpecUserOverview id={Number(params.userId)} />
-    </div>
+    <>
+      <div className="h-[80vh] w-full flex items-center justify-evenly flex-col text-3xl font-semibold">
+        <SpecUserOverview id={Number(params.userId)} />
+      </div>
+      {username == author?.userName && (
+        <Link
+          href={`/dashboard/users/${params.userId}/edit`}
+          className="rounded-3xl border border-green-500 p-3 bg-green-300 font-semibold hover:bg-green-400"
+        >
+          Edit user properties
+        </Link>
+      )}
+    </>
   );
 }
