0112608f82660c86588f021f5af352193a61a2e1
Majdanka <majdan_k@onet.pl>
2024-05-24 12:53:37 +0200

    Gallery page responsive changes

--- app/(publicPage)/gallery/page.tsx
+++ app/(publicPage)/gallery/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 
 export default function Gallery() {
   return (
-    <main className="w-[99%] bg-orange-300 rounded-3xl md:h-[80vh] flex flex-col items-center h-[140vh]">
+    <main className="w-[99%] bg-orange-300 rounded-3xl md:h-[80vh] md:p-0 flex flex-col items-center h-fit p-5">
       <h1 className="text-4xl font-bold pt-2">Gallery</h1>
       <Suspense fallback="Loading gallery... ">
         <GalleryHandler />
